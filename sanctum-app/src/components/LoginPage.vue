5b62b3363aaf4852cbd9da2f1cb3d7699de23f2f
delapenaroberto <tutorabegil18@gmail.com>
2024-05-22 21:01:43 +0800

    Commit

--- sanctum-app/src/components/LoginPage.vue
+++ sanctum-app/src/components/LoginPage.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="login-container">
-    <div class="container mt-5 col-3">
-      <div class="row">
-        <h4>ENTER YOUR EMAIL AND PASSWORD</h4>
-        <form @submit.prevent="loginUser" class="col-12">
-          <div class="form-group mt-3">
-            <input type="email" class="form-control" id="email" v-model="email" placeholder="Enter email" required @input="clearErrors">
-            <small class="text-danger" v-if="errors">{{ errors }}</small>
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title text-center">ENTER YOUR EMAIL AND PASSWORD</h4>
+              <form @submit.prevent="loginUser">
+                <div class="form-group">
+                  <input type="email" class="form-control" id="email" v-model="email" placeholder="Enter email" required @input="clearErrors">
+                  <small class="text-danger" v-if="errors">{{ errors }}</small>
+                </div><br>
+                <div class="form-group">
+                  <input type="password" class="form-control" id="password" v-model="password" placeholder="Password" required @input="clearErrors">
+                </div><br>
+                <div class="text-center"> <!-- Added class for centering -->
+                  <button type="submit" class="btn btn-primary btn-block">Login</button>
+                </div>
+              </form>
+              <p class="mt-3 text-center">Don't have an account? <router-link to="/register">Register here!</router-link></p>
+            </div>
           </div>
-          <div class="form-group mt-3">
-            <input type="password" class="form-control" id="password" v-model="password" placeholder="Password" required @input="clearErrors">
-          </div>
-          <button type="submit" class="btn btn-primary mt-3 w-100">Login</button>
-        </form>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <p class="mt-3 text-left">Don't have an account? <router-link to="/register">Register here!</router-link></p>
         </div>
       </div>
     </div>
@@ -57,16 +61,17 @@ export default {
 </script>
 
 <style scoped>
-.login-container {
-  background-image: url('../assets/download\ \(2\).jpg');
-  background-size: cover;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+
+
+.card {
+  border: none;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 }
 
-h4{
-  text-align: center;
+.card-title {
+  color: #333;
 }
+
+/* Add additional styles as needed */
 </style>
