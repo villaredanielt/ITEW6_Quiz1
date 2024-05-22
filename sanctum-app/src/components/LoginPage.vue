14377f4396be8a78a443db6ee036449668c2790c
delapenaroberto <tutorabegil18@gmail.com>
2024-05-23 01:30:44 +0800

    Commit on Login Page Design

--- sanctum-app/src/components/LoginPage.vue
+++ sanctum-app/src/components/LoginPage.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="login-container">
     <div class="container mt-5">
-      <div class="row justify-content-center">
+      <div class="row justify-content-start"> <!-- Adjusted to align the card to the left -->
         <div class="col-md-6">
+          <img src="https://www.psdgraphics.com/file/group-of-people.jpg" alt="Logo" class="logo">
+          <div class="card"></div>
           <div class="card">
             <div class="card-body">
               <h4 class="card-title text-center">ENTER YOUR EMAIL AND PASSWORD</h4>
@@ -14,7 +16,7 @@
                 <div class="form-group">
                   <input type="password" class="form-control" id="password" v-model="password" placeholder="Password" required @input="clearErrors">
                 </div><br>
-                <div class="text-center"> <!-- Added class for centering -->
+                <div class="text-center">
                   <button type="submit" class="btn btn-primary btn-block">Login</button>
                 </div>
               </form>
@@ -61,17 +63,36 @@ export default {
 </script>
 
 <style scoped>
-
+.login-container {
+  background-image: url('C:\Abegil\ITEW6_Quiz1\sanctum-app\src\image\social-network-modern-flat-concept-web-banner-design-woman-communicates-online-chats_9209-8395.jpg');
+  background-size: 800px 750px; /* Set background size */
+  background-position: right; /* Adjusted to position the image on the right side */
+  background-repeat: no-repeat;
+  height: 100vh; /* Set the height of the container to 100% of the viewport height */
+  display: flex;
+  margin-right: 0px;
+  justify-content: left; /* Adjusted to align the card to the left */
+  align-items: center;
+}
 
 .card {
+  background-color: rgb(135, 190, 235);
   border: none;
   border-radius: 10px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  margin-right: 50px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1); /* Added box shadow */
 }
 
 .card-title {
   color: #333;
 }
 
+.logo {
+  max-width: 15%; /* Ensure the logo doesn't exceed its container */
+  position: absolute; /* Position the logo absolutely */
+  top: 50%; /* Position from the top */
+  left: 50%; /* Position from the left */
+  transform: translate(-210%, -180%); /* Center the logo */
+}
 /* Add additional styles as needed */
 </style>
